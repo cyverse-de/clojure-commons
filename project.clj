e806f089af703545847b52e24d95ba3286ca6148
Ian McEwen <mian@cyverse.org>
2016-08-30 14:25:18 -0700

    Version 2.8.0 (+switch to org.cyverse)

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject org.iplantc/clojure-commons "5.2.8.0"
+(defproject org.cyverse/clojure-commons "2.8.0"
   :description "Common Utilities for Clojure Projects"
-  :url "https://github.com/iPlantCollaborativeOpenSource/DE"
+  :url "https://github.com/cyverse-de/iplant-clojure-commons"
   :license {:name "BSD"
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :plugins [[test2junit "1.1.3"]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [buddy/buddy-sign "0.7.0"]
@@ -20,5 +22,5 @@
                  [ring "1.4.0"]
                  [slingshot "0.12.2"]
                  [trptcolin/versioneer "0.2.0"]
-                 [org.iplantc/service-logging "5.2.8.0"]]
+                 [org.cyverse/service-logging "2.8.0"]]
   :profiles {:test {:resource-paths ["resources" "test-resources"]}})
