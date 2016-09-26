57b7c1f529b092db48d8e8e3a30730a78c50e5a3
Ian McEwen <mian@cyverse.org>
2016-09-26 10:46:57 -0700

    Version 2.8.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/clojure-commons "2.8.1-SNAPSHOT"
+(defproject org.cyverse/clojure-commons "2.8.1"
   :description "Common Utilities for Clojure Projects"
   :url "https://github.com/cyverse-de/iplant-clojure-commons"
   :license {:name "BSD"
