cfb9d8bdef9c098e0b6a8148ec5649afa18bdd59
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-03-11 09:28:12 -0700

    Add when-let* macro to clojure-commons.core

--- src/clojure_commons/core.clj
+++ src/clojure_commons/core.clj
@@ -7,3 +7,11 @@
   [m]
   (assert (map? m) "the argument to remove-nil-values must be a map")
   (remove-vals nil? m))
+
+;; Taken from https://clojuredocs.org/clojure.core/when-let#example-5797f908e4b0bafd3e2a04bb
+(defmacro when-let*
+  ([bindings & body]
+   (if (seq bindings)
+     `(when-let [~(first bindings) ~(second bindings)]
+        (when-let* ~(drop 2 bindings) ~@body))
+     `(do ~@body))))
