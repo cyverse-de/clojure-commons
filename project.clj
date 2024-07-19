22f0acdcfa3489cfb1e9d14401236a25fda8b1a3
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-19 11:51:54 -0700

    updated the version to 3.0.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/clojure-commons "3.0.8"
+(defproject org.cyverse/clojure-commons "3.0.9-SNAPSHOT"
   :description "Common Utilities for Clojure Projects"
   :url "https://github.com/cyverse-de/clojure-commons"
   :license {:name "BSD"
