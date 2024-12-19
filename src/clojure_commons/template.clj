d2ca4af7dcfd984fd7d9e2a16555d901fcd59bc6
Paul Sarando <psarando@cyverse.org>
2024-12-19 14:09:16 -0700

    CORE-2021 Fix class cast exceptions in template/render
    Fixes exceptions when template values contain numbers or UUIDs,
    such as in the apps service sharing notification messages.

--- src/clojure_commons/template.clj
+++ src/clojure_commons/template.clj
@@ -4,4 +4,4 @@
 (defn render
   "Takes a format string and a map of interpolation values to substitute into the string."
   [fmt m]
-  (string/replace fmt #"\{\{([^\}]+)\}\}" (fn [[orig k]] (get m (keyword k) orig))))
+  (string/replace fmt #"\{\{([^\}]+)\}\}" (fn [[orig k]] (str (get m (keyword k) orig)))))
--- test/clojure_commons/template_test.clj
+++ test/clojure_commons/template_test.clj
@@ -18,7 +18,11 @@
                {:desc     "empty template string"
                 :fmt      "This is a {{foo}} of the {{}}."
                 :vals     {:foo "test"}
-                :expected "This is a test of the {{}}."}]]
+                :expected "This is a test of the {{}}."}
+               {:desc     "number and UUID values"
+                :fmt      "This is test {{foo}} for UUID {{bar}}."
+                :vals     {:foo 5 :bar (parse-uuid "feedface-cafe-b0ba-feed-facedeadbeaf")}
+                :expected "This is test 5 for UUID feedface-cafe-b0ba-feed-facedeadbeaf."}]]
     (doseq [{:keys [desc fmt vals expected]} cases]
       (testing desc
         (is (= expected (render fmt vals)))))))
