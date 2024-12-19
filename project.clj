210174e648d438d35617aac0bb38f13f27a6ad90
Paul Sarando <psarando@cyverse.org>
2024-12-19 15:24:23 -0700

    Version 3.0.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/clojure-commons "3.0.10-SNAPSHOT"
+(defproject org.cyverse/clojure-commons "3.0.10"
   :description "Common Utilities for Clojure Projects"
   :url "https://github.com/cyverse-de/clojure-commons"
   :license {:name "BSD"
