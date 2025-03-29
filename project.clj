b86d71dc3feaed95503dad61bb9d2c34047668bc
Sarah Roberts <sonoran.sarah@gmail.com>
2025-03-28 18:38:28 -0700

    bumped the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/clojure-commons "3.0.11"
+(defproject org.cyverse/clojure-commons "3.0.12-SNAPSHOT"
   :description "Common Utilities for Clojure Projects"
   :url "https://github.com/cyverse-de/clojure-commons"
   :license {:name "BSD"
